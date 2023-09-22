2dc03f19a609563bf3cbb34088ab46e8e05322cc
COBBINA EMMANUEL ADU BOATENG <aduboateng333@gmail.com>
2023-09-22 13:54:01 +0000

    recap

--- myapp/views.py
+++ myapp/views.py
@@ -2,15 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 
+
 def index(request):
-    context = {
-        'name' : "Adu Boat",
-        'age' : 24,
-        'Nationality': 'Togolese'
-    }
-    return(render(request, 'index.html', context))
+    context = {"name": "Adu Boat", "age": 24, "Nationality": "Togolese"}
+    return render(request, "index.html", context)
+
 
 def counter(request):
-    texts = request.POST['text']
+    texts = request.POST["text"]
     number_of_words = len(texts.split())
-    return(render(request, 'counter.html', {'num':number_of_words}))
+    return render(request, "counter.html", {"num": number_of_words})
