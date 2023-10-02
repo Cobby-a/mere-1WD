388b4f22d94586c35412eff68051b08e419f4d49
COBBINA EMMANUEL ADU BOATENG <aduboateng333@gmail.com>
2023-10-02 06:49:26 +0000

    added the models to our views file

--- myapp/views.py
+++ myapp/views.py
@@ -1,38 +1,11 @@
 from django.shortcuts import render
-from . import models
+from .models import features
 # Create your views here.
 
 
 def index(request):
-    feature1 = models.features()
-    feature1.id = 1
-    feature1.author= 'Adu Boateng'
-    feature1.about= 'iuire uufiruoi oiwejoi iewi'
-    feature1.pages= 45
-    feature1.is_true = True
+    featuress = features.objects.all()
 
-    feature2 = models.features()
-    feature2.id = 2
-    feature2.author= 'Cobby Drew'
-    feature2.about= "yess we are on what we've been doing the most blud"
-    feature2.pages= 56
-    feature2.is_true = False
-
-    feature3 = models.features()
-    feature3.id = 3
-    feature3.author= 'Ankobam Kyere'
-    feature3.about= 'kjiue i984 8u9u43 iuhuds ihuei'
-    feature3.pages= 34
-    feature3.is_true = True
-
-    feature4 = models.features()
-    feature4.id = 4
-    feature4.author= 'David Cobbina'
-    feature4.about= ' 4u 9iri iuire uufiruoi oiwejoi iewi'
-    feature4.pages= 23
-    feature4.is_true = True
-
-    featuress = [feature1, feature2, feature3, feature4]
     context = {"name": "Adu Boat", "age": 24, "Nationality": "Togolese"}
     return render(request, "index.html", {'feature': featuress})
 
