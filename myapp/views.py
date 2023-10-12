7f75f6cd6fe5fedcb1c65afb6c998b5ebc757e12
COBBINA EMMANUEL ADU BOATENG <aduboateng333@gmail.com>
2023-10-12 16:03:17 +0000

    signup authentication

--- myapp/views.py
+++ myapp/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import features
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 # Create your views here.
 
 
@@ -14,3 +16,26 @@ def counter(request):
     texts = request.POST["text"]
     number_of_words = len(texts.split())
     return render(request, "counter.html", {"num": number_of_words})
+
+def register(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(username = username).exists():
+                messages.info(request, 'Username already exists')
+                return redirect('register')
+            elif User.objects.filter(email = email).exists():
+                messages.info(request, 'Email already exists')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'Password does not match')
+    else:
+        return render(request, 'register.html')
