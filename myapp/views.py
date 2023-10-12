cb976929f78b8e4c2f7e0cc134008e8a5d68785d
COBBINA EMMANUEL ADU BOATENG <aduboateng333@gmail.com>
2023-10-12 18:32:58 +0000

    login authentication

--- myapp/views.py
+++ myapp/views.py
@@ -39,3 +39,18 @@ def register(request):
             messages.info(request, 'Password does not match')
     else:
         return render(request, 'register.html')
+    
+def login(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username = username, password = password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Invalid Credentials')
+            return redirect('login')
+    else:
+        return render(request, 'login.html')
