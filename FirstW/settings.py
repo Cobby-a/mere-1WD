3afcca39a3816b04d7138cb99fe1e5b550ecd704
COBBINA EMMANUEL ADU BOATENG <aduboateng333@gmail.com>
2023-10-20 11:44:46 +0000

    configured postgres our database

--- FirstW/settings.py
+++ FirstW/settings.py
@@ -77,8 +77,11 @@ WSGI_APPLICATION = "FirstW.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "myproject1",
+        "USER": "postgres",
+        "PASSWORD": "0243",
+        "HOST":"localhost",
     }
 }
 
